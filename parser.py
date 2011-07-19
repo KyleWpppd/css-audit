0751f71789ae8a033120256f3043e19ea1ecc38e
Kyle Wanamaker <kw@renttherunway.com>
2011-07-19 12:59:04 -0400

    Right now can get all of the styles referenced in a document. Maybe should create some test documents and verify that they work properly.

--- parser.py
+++ parser.py
@@ -5,13 +5,31 @@ CSS Audit
 
 from HTMLParser import HTMLParser
 
-class cssaudit(HTMLParser):
+class Cssparser(HTMLParser):
     def __init__(self, fh):
         HTMLParser.__init__(self)
+        self.css_classes = []
         self.fileids = []
         self.feed(fh.read())
 
+    
     def handle_starttag(self, tag, attrs):
-        print attrs
+        self.append_styles(tag, attrs)
+    
+    def handle_startendtag(self, tag, attrs):
+        self.append_styles(tag, attrs)
+    
+    def append_styles(self, tag, attrs):
+        dattrs = dict(attrs)
+        if 'class' in dattrs:
+            print "Found classes '%s'" % dattrs['class']
+            self.css_classes.append(dattrs['class'])
+
+
+
+# must send a file to open!
+f = open('/Users/kyle/src/css-audit/data/yahoo.html', 'r')
+mycssauditor = Cssparser(f)
+print "Total classes found %s" % mycssauditor.css_classes
 
 
