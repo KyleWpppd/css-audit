fb6cb1c7d7eba543a1153e5b2392d556734d1a8a
Kyle Wanamaker <kw@renttherunway.com>
2011-07-30 17:43:44 -0400

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,32 @@
+# Setup.py for cssaudit
+# (c) 2011, Kyle Wanamaker
+# Licensed under the GNU GPL v3
+
+from setuptools import setup
+
+setup(
+      name='CssAudit',
+      packages=find_packages(),
+      version='0.01a',
+      description='A rather basic tool to see if you have excess styles in your CSS stylesheets.',
+      author='Kyle Wanamaker (KyleWpppd)',
+      author_email='k.dev@klowell.com',
+      url='http://github.com/KyleWpppd/css-audit',
+      install_requires=['cssutils>=0.9.8a3', ],
+      classifiers=[
+          'Development Status :: 2 - Pre-Alpha'
+          'Environment :: Console',
+          'Environment :: Web Environment',
+          'Intended Audience :: End Users/Desktop',
+          'Intended Audience :: Developers',
+          'Intended Audience :: System Administrators',
+          'License :: OSI Approved :: GNU General Public License (GPL)'
+          'Operating System :: OS Independent'
+          'Operating System :: MacOS :: MacOS X',
+          'Operating System :: Microsoft :: Windows',
+          'Operating System :: POSIX',
+          'Programming Language :: Python',
+          'Topic :: Utilities'
+          ],
+
+      )
